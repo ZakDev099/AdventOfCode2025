1e4c0bd80866fb5d7b7bb7354556fc104babd602
Zak Dyer <30128282@tafe.wa.edu.au>
2025-12-03 17:02:49 +0800

    Completed Day 2 Puzzle 1

---
+++ Day2/Puzzle1/ID_Validator.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day2;
+
+public class ID_Validator
+{
+    public List<long> Valid_IDs = [];
+    public List<long> Invalid_IDs = [];
+    private List<(long floor, long ceiling)> id_Ranges = [];
+
+    public ID_Validator(string[] data)
+    {
+        foreach (string ss in data)
+        {
+            if (ValidateRange(ss, out var result)) 
+            {
+                id_Ranges.Add(result);
+            }
+        }
+
+        foreach (var id_Range in id_Ranges)
+        {
+            for (long i = id_Range.floor; i <= id_Range.ceiling; i++)
+            {
+                switch (Validate_ID(i))
+                {
+                    case true:
+                        Valid_IDs.Add(i);
+                        break;
+
+                    case false:
+                        Invalid_IDs.Add(i);
+                        break;
+                }
+            }
+        }
+    }
+
+
+    // Removes whitespace and checks if the string is empty, verifies the string characters are valid,
+    // then splits the string into an integer pair
+    public static bool ValidateRange(string input, out (long floor, long ceiling) result)
+    {
+        result = new();
+        if (string.IsNullOrWhiteSpace(input)) 
+            return false;
+        
+        string validatedString = string.Empty;
+        foreach (char i in input)
+        {
+            // Confirming the new string has only numbers and hyphens
+            if (char.IsAsciiDigit(i) || i == '-')
+                validatedString += i;
+        }
+
+        var stringParts = validatedString.Split('-');
+
+        if(stringParts.Length == 2 && stringParts[0].Length > 0 && stringParts[1].Length > 0)
+        {
+            result.floor = long.Parse(stringParts[0]);
+            result.ceiling = long.Parse(stringParts[1]);
+            return true;
+        }
+        else 
+            return false;
+        
+    }
+
+    public static bool Validate_ID(long id_num)
+    {
+        string id = id_num.ToString();
+
+        // Check to see if ID starts with a 0 -> invalid
+        if (id[0] == '0') 
+            return false;
+
+        // Check to see if ID is odd length -> valid
+        if (id.Length % 2 == 1) 
+            return true;
+
+        // Split ID into two halves and check if they are equal
+        string firstHalf = id[..(id.Length / 2)];
+        string secondHalf = id[(id.Length / 2)..];
+
+        if (firstHalf == secondHalf) 
+            return false;
+
+        // Finally, ID is valid if no other conditions were met
+        return true;
+    }
+}
---
+++ Day2/Puzzle1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using Microsoft.VisualBasic;
+
+namespace Day2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader("inputSource.txt"))
+            {
+                string[] substrings = sr.ReadToEnd().Split(',');
+                ID_Validator id_Val = new(substrings);
+
+                long invalidSum = 0;
+                foreach (long id in id_Val.Invalid_IDs)
+                {
+                    invalidSum += id;
+                }
+
+                Console.WriteLine($"The total sum of invalid ID's is: {invalidSum}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+    }
+}
