593b0cc5fd3fbd3135fd92fed2fa8be4f159937f
Zak Dyer <30128282@tafe.wa.edu.au>
2025-12-05 16:47:12 +0800

    Add Day 4 Puzzle 1
    Need to fix IndexOutOfBounds bug

---
+++ Day4/Day4Puzzle1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+namespace Day4Puzzle1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int accessibleRolls = 0;
+        int accessibilityRequirement = 3;
+        bool[,]? mineField = null;
+
+        try
+        {
+            using (StreamReader sr = new StreamReader("inputSource.txt"))
+            {
+                string? line;
+                char mine = '@';
+                List<List<bool>> rows = [];
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    List<bool> columns = [];
+
+                    foreach (char c in line)
+                    {
+                        if (c == mine)
+                        {
+                            columns.Add(true);
+                        }
+                        else
+                        {
+                            columns.Add(false);
+                        }
+                    }
+
+                    rows.Add(columns);
+                }
+
+                mineField = new bool[rows.Count, rows[0].Count];
+
+                for (int row = 0; row < rows.Count; row++)
+                {
+                    for (int col = 0; col < rows[row].Count; col++)
+                    {
+                        try
+                        {
+                            mineField[row, col] = rows[row][col];
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("ERROR:: Grid shape must be rectangular");
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        if (mineField != null)
+        {
+            Minefield mines = new(mineField);
+            MineSweeper mineSweeper = new(mines);
+
+            foreach (bool mine in mineSweeper.Minefield.Mines)
+            {
+                if (mine)
+                {
+                    if (mineSweeper.SweepImmediateArea(1) <= accessibilityRequirement)
+                    {
+                        accessibleRolls++;
+                    }
+
+                    mineSweeper.StepDown();
+                }
+            }
+        }
+
+        Console.WriteLine(accessibleRolls);
+    }
+}
