307fdd6f856499df1e175f108f0a5ce9224581db
Zak Dyer <30128282@tafe.wa.edu.au>
2025-12-08 17:40:08 +0800

    added: main
    (wip)

---
+++ Day5/Day5Puzzle1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Day5Puzzle1;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader("inputSource.txt"))
+            {
+                List<int> id_List = [];
+                List<Range> id_Ranges = [];
+
+                string? line;
+
+                while (!string.IsNullOrWhiteSpace(line = sr.ReadLine()))
+                {
+                    if (Utils.TrySplitStringToRange(line, '-', out var id_Range))
+                    {
+                        id_Ranges.Add((Range)id_Range!);
+                    }
+                    else if (int.TryParse(line, out int id))
+                    {
+                        id_List.Add(id);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: failed to parse a line in input");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
