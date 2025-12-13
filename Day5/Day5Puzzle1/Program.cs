be0a2a82b04146eacbf942c617861d310bc6c1f8
Zak Dyer <30128282@tafe.wa.edu.au>
2025-12-13 16:20:56 +0800

    solved: Day 5 Puzzle 1

--- Day5/Day5Puzzle1/Program.cs
+++ Day5/Day5Puzzle1/Program.cs
@@ -6,24 +6,29 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        List<long> id_List = [];
+        List<(long min, long max)?> id_Ranges = [];
+
         try
         {
             using (StreamReader sr = new StreamReader("inputSource.txt"))
             {
-                List<int> id_List = [];
-                List<Range> id_Ranges = [];
-
                 string? line;
 
-                while (!string.IsNullOrWhiteSpace(line = sr.ReadLine()))
+                while ((line = sr.ReadLine()) != null)
                 {
                     if (Utils.TrySplitStringToRange(line, '-', out var id_Range))
                     {
-                        id_Ranges.Add((Range)id_Range!);
+                        id_Ranges.Add(id_Range);
                     }
-                    else if (int.TryParse(line, out int id))
+                    else if (long.TryParse(line, out long id))
                     {
                         id_List.Add(id);
+                    }
+                    else if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
                     }
                     else
                     {
@@ -36,5 +41,33 @@ class Program
         {
             Console.WriteLine(ex);
         }
+
+
+
+        if (id_List == null)
+        {
+            Console.WriteLine("Error: failed to load IDs");
+            return;
+        }
+        else if (id_Ranges == null)
+        {
+            Console.WriteLine("Error: failed to load ID Ranges");
+            return;
+        }
+
+        int validIdCount = 0;
+        foreach (long id in id_List)
+        {
+            foreach ((long min, long max) rng in id_Ranges)
+            {
+                if (rng.min <= id && id <= rng.max)
+                {
+                    validIdCount++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(validIdCount);
     }
 }
--- Day5/Day5Puzzle1/Utils.cs
+++ Day5/Day5Puzzle1/Utils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace Day5Puzzle1
 {
     public static class Utils
     {
-        public static bool TrySplitStringToRange(string input, char delimiter, out Range? id_Range)
+        public static bool TrySplitStringToRange(string input, char delimiter, out (long min, long max)? id_Range)
         {
             id_Range = null;
 
@@ -15,10 +16,10 @@ namespace Day5Puzzle1
             {
                 var splitLine = input.Split('-');
 
-                if (!int.TryParse(splitLine[0], out int val1) || !int.TryParse(splitLine[1], out int val2)) 
+                if (!long.TryParse(splitLine[0], out long val1) || !long.TryParse(splitLine[1], out long val2)) 
                     return false;
                 else
-                    id_Range = (val1 > val2) ? val2..val1 : val1..val2;
+                    id_Range = (val1 > val2) ? id_Range = (val2, val1) : id_Range = (val1, val2);
                     return true;
             }
             else return false;
