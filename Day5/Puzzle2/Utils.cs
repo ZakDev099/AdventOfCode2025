e5b31daaf306e87759be846f6bbd807e78b5f8a3
Zak Dyer <30128282@tafe.wa.edu.au>
2025-12-13 16:32:54 +0800

    updated: solution
    + copied Day5Puzzle 1 -> Day5Puzzle2

---
+++ Day5/Puzzle2/Utils.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day5Puzzle2
+{
+    public static class Utils
+    {
+        public static bool TrySplitStringToRange(string input, char delimiter, out (long min, long max)? id_Range)
+        {
+            id_Range = null;
+
+            if (input.Contains(delimiter))
+            {
+                var splitLine = input.Split('-');
+
+                if (!long.TryParse(splitLine[0], out long val1) || !long.TryParse(splitLine[1], out long val2)) 
+                    return false;
+                else
+                    id_Range = (val1 > val2) ? id_Range = (val2, val1) : id_Range = (val1, val2);
+                    return true;
+            }
+            else return false;
+        }
+    }
+}
